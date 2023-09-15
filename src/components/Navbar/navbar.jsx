b3fdc425541e2cd4352eb8e3cf8a6adf3c64784f
Romina Manzano <manzano.romina13@gmail.com>
2023-09-15 19:42:58 -0300

    DELETE: unused pages and updated navbar

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 import appData from "data/app.json";
-import { handleDropdown, handleMobileDropdown } from "common/navbar";
+import { handleMobileDropdown } from "common/navbar";
 
 const Navbar = ({ lr, nr, theme }) => {
   return (
@@ -13,15 +13,7 @@ const Navbar = ({ lr, nr, theme }) => {
     >
       <div className="container">
         <Link to="/" className="logo">
-          {theme ? (
-            theme === "themeL" ? (
-              <img ref={lr} src={appData.darkLogo} alt="logo" />
-            ) : (
-              <img ref={lr} src={appData.lightLogo} alt="logo" />
-            )
-          ) : (
-            <img ref={lr} src={appData.lightLogo} alt="logo" />
-          )}
+          <img ref={lr} src={appData.lightLogo} alt="logo" />
         </Link>
 
         <button
@@ -41,107 +33,24 @@ const Navbar = ({ lr, nr, theme }) => {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Home
-              </span>
-              <div className="dropdown-menu">
-                <Link to={`/homepage/home1-dark`} className="dropdown-item">
-                  Main Home
-                </Link>
-                <Link to={`/homepage/home2-dark`} className="dropdown-item">
-                  Creative Agency
-                </Link>
-                <Link to={`/homepage/home5-dark`} className="dropdown-item">
-                  Digital Agency
-                </Link>
-                <Link to={`/homepage/home4-dark`} className="dropdown-item">
-                  Business One Page
-                </Link>
-                <Link to={`/homepage/home3-dark`} className="dropdown-item">
-                  Corporate Business
-                </Link>
-                <Link to={`/homepage/home6-dark`} className="dropdown-item">
-                  Modern Agency
-                </Link>
-                <Link to={`/homepage/home7-dark`} className="dropdown-item">
-                  Freelancer
-                </Link>
-                <Link to={`/homepage/home8-dark`} className="dropdown-item">
-                  Architecture
-                </Link>
-              </div>
-            </li>
-            <li className="nav-item">
-              <Link to={`/about/about-dark`} className="nav-link">
-                About
+            <li className="nav-item dropdown">
+              <Link to={`/`} className="nav-link">
+                Inicio
               </Link>
             </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Works
-              </span>
-              <div className="dropdown-menu">
-                <Link to={`/showcase/showcase-dark`} className="dropdown-item">
-                  Showcase Parallax
-                </Link>
-                <Link to={`/showcase4/showcase4-dark`} className="dropdown-item">
-                  Showcase Carousel
-                </Link>
-                <Link to={`/showcase3/showcase3-dark`} className="dropdown-item">
-                  Showcase Circle
-                </Link>
-                <Link to={`/works/works-dark`} className="dropdown-item">
-                  Portfolio Masonry
-                </Link>
-                <Link to={`/works2/works2-dark`} className="dropdown-item">
-                  Portfolio Filtering
-                </Link>
-                <Link to={`/works3/works3-dark`} className="dropdown-item">
-                  Portfolio Gallery
-                </Link>
-              </div>
+            <li className="nav-item">
+              <Link to={`/about`} className="nav-link">
+                Sobre Nosotros
+              </Link>
             </li>
-            <li className="nav-item dropdown" onClick={handleDropdown}>
-              <span
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Blog
-              </span>
-              <div className="dropdown-menu">
-                <Link to={`/blog/blog-dark`} className="dropdown-item">
-                  Blog Standerd
-                </Link>
-                <Link to={`/blog-list/blog-list-dark`} className="dropdown-item">
-                  Blog List
-                </Link>
-                <Link to={`/blog-grid/blog-grid-dark`} className="dropdown-item">
-                  Blog Grid
-                </Link>
-                <Link to={`/blog-details/blog-details-dark`} className="dropdown-item">
-                  Blog Details
-                </Link>
-              </div>
+            <li className="nav-item dropdown">
+              <Link to={`/meet-ups`} className="nav-link">
+                Meet Ups
+              </Link>
             </li>
             <li className="nav-item">
-              <Link to={`/contact/contact-dark`} className="nav-link">
-                Contact
+              <Link to={`/contact`} className="nav-link">
+                Contacto
               </Link>
             </li>
           </ul>
