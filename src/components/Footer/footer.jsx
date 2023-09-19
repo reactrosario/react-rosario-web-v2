226ca713156b2e16935d44e2cab7628fe522f953
Romina Manzano <manzano.romina13@gmail.com>
2023-09-19 09:41:34 -0300

    UPDATE: footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,127 +1,115 @@
 import React from "react";
-import { Link } from 'gatsby'
 import appData from "data/app.json";
 
-const Footer = ({ hideBGCOLOR }) => {
-  return (
-    <footer className={`${!hideBGCOLOR ? "sub-bg" : ""}`}>
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-4">
-            <div className="item md-mb50">
-              <div className="title">
-                <h5>Contact Us</h5>
-              </div>
-              <ul>
-                <li>
-                  <span className="icon pe-7s-map-marker"></span>
-                  <div className="cont">
-                    <h6>Officeal Address</h6>
-                    <p>504 White St . Dawsonville, GA 30534 , New York</p>
-                  </div>
-                </li>
-                <li>
-                  <span className="icon pe-7s-mail"></span>
-                  <div className="cont">
-                    <h6>Email Us</h6>
-                    <p>support@gmail.com</p>
-                  </div>
-                </li>
-                <li>
-                  <span className="icon pe-7s-call"></span>
-                  <div className="cont">
-                    <h6>Call Us</h6>
-                    <p>+87986451666</p>
-                  </div>
-                </li>
-              </ul>
+const Footer = ({ hideBGCOLOR }) => (
+  <footer className={`${!hideBGCOLOR ? "sub-bg" : ""}`}>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="item md-mb50">
+            <div className="title">
+              <h5>Contacto</h5>
             </div>
+            <ul>
+              <li>
+                <span className="icon pe-7s-mail"></span>
+                <div className="cont">
+                  <h6>Email</h6>
+                  <p>reactrosario@gmail.com</p>
+                </div>
+              </li>
+              <li>
+                <span className="icon pe-7s-share"></span>
+                <div className="cont">
+                  <h6>Redes sociales</h6>
+                  <p>@reactrosario</p>
+                </div>
+              </li>
+            </ul>
           </div>
-          <div className="col-lg-4">
-            <div className="item md-mb50">
-              <div className="title">
-                <h5>Recent News</h5>
-              </div>
-              <ul>
-                <li>
-                  <div className="img">
-                    <Link to="/blog-details/blog-details-dark">
-                      <img src="/img/blog/1.jpg" alt="" />
-                    </Link>
-                  </div>
-                  <div className="sm-post">
-                    <Link to="/blog-details/blog-details-dark">
-                      <p>
-                        The Start-Up Ultimate Guide to Make Your WordPress
-                        Journal.
-                      </p>
-                    </Link>
-                    <Link to="/blog/blog-dark">
-                      <span className="date">14 sep 2022</span>
-                    </Link>
-                  </div>
-                </li>
-                <li>
-                  <div className="img">
-                    <Link to="/blog-details/blog-details-dark">
-                      <img src="/img/blog/2.jpg" alt="" />
-                    </Link>
-                  </div>
-                  <div className="sm-post">
-                    <Link to="/blog-details/blog-details-dark">
-                      <p>
-                        The Start-Up Ultimate Guide to Make Your WordPress
-                        Journal.
-                      </p>
-                    </Link>
-                    <Link to="/blog/blog-dark">
-                      <span className="date">14 sep 2022</span>
-                    </Link>
-                  </div>
-                </li>
-                <li>
-                  <div className="subscribe">
-                    <input type="text" placeholder="Type Your Email" />
-                    <span className="subs pe-7s-paper-plane"></span>
-                  </div>
-                </li>
-              </ul>
+        </div>
+
+        <div className="col-lg-4">
+          {/* <div className="item md-mb50">
+            <div className="title">
+              <h5>Recent News</h5>
             </div>
-          </div>
-          <div className="col-lg-4">
-            <div className="item">
-              <div className="logo">
-                <img src={appData.lightLogo} alt="" />
-              </div>
-              <div className="social">
-                <a href="#0">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href="#0">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="#0">
-                  <i className="fab fa-instagram"></i>
-                </a>
-                <a href="#0">
-                  <i className="fab fa-youtube"></i>
-                </a>
-              </div>
-              <div className="copy-right">
-                <p>
-                  © 2022, Vie Template. Made with passion by
-                  <Link to="https://themeforest.net/user/themescamp/portfolio" target="_blank">
-                    ThemesCamp
+            <ul>
+              <li>
+                <div className="img">
+                  <Link to="/blog-details/blog-details-dark">
+                    <img src="/img/blog/1.jpg" alt="" />
                   </Link>
-                  .
-                </p>
-              </div>
+                </div>
+                <div className="sm-post">
+                  <Link to="/blog-details/blog-details-dark">
+                    <p>
+                      The Start-Up Ultimate Guide to Make Your WordPress
+                      Journal.
+                    </p>
+                  </Link>
+                  <Link to="/blog/blog-dark">
+                    <span className="date">14 sep 2022</span>
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="img">
+                  <Link to="/blog-details/blog-details-dark">
+                    <img src="/img/blog/2.jpg" alt="" />
+                  </Link>
+                </div>
+                <div className="sm-post">
+                  <Link to="/blog-details/blog-details-dark">
+                    <p>
+                      The Start-Up Ultimate Guide to Make Your WordPress
+                      Journal.
+                    </p>
+                  </Link>
+                  <Link to="/blog/blog-dark">
+                    <span className="date">14 sep 2022</span>
+                  </Link>
+                </div>
+              </li>
+              <li>
+                <div className="subscribe">
+                  <input type="text" placeholder="Type Your Email" />
+                  <span className="subs pe-7s-paper-plane"></span>
+                </div>
+              </li>
+            </ul>
+          </div> */}
+        </div>
+
+        <div className="col-lg-4">
+          <div className="item">
+            <div className="logo">
+              <img src={appData.lightLogo} alt="" />
+            </div>
+            <div className="social">
+              <a href="https://www.meetup.com/es/reactrosario/" target="_blank" rel="noreferrer">
+                <i className="fab fa-meetup"></i>
+              </a>
+              <a href="https://twitter.com/reactrosario" target="_blank" rel="noreferrer">
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a href="https://www.instagram.com/reactrosario" target="_blank" rel="noreferrer">
+                <i className="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.youtube.com/@reactrosario7085" target="_blank" rel="noreferrer">
+                <i className="fab fa-youtube"></i>
+              </a>
+            </div>
+            <div className="copy-right">
+              <p>
+                © {new Date().getFullYear()}, React Rosario
+              </p>
             </div>
           </div>
         </div>
       </div>
-    </footer>
-  );
-};
+    </div>
+  </footer>
+);
 
 export default Footer;
