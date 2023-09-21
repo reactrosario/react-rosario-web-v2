e76064354271c7201fc6a365414870e3f502b9aa
Romina Manzano <manzano.romina13@gmail.com>
2023-09-21 10:36:03 -0300

    FIX: deminished blocking resources loading

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,15 +11,19 @@ const HeadComponents = [
   <meta key="keywords" name="keywords" content="React ReactJS Rosario Comunidad Meetups Meet Up" />,
   <meta key="description" name="description" content="Una comunidad de desarrolladores ReactJS y más" />,
   <meta key="author" name="author" content="React Rosario" />,
+
   // Google Fonts
-  <link key="Poppins" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" />,
+  <link key="Poppins" rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" />,
   <link key="Montserrat" rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap" />,
+  
   // Styles
-  <link key="ionicons" rel="stylesheet" href="/css/ionicons.min.css" />,
+  <link key="ionicons" rel="preload" as="style" href="/css/ionicons.min.css" />,
   <link key="styles-light" rel="stylesheet" href="/css/light.css" />,
   <link key="styles-dark" rel="stylesheet" href="/css/dark.css" />,
+  
   // Icon
   <link key="icon" rel="shortcut icon" href="/img/favicon.ico" title="Favicon" sizes="16x16" />,
+  
   // Scripts
   <script key="isotope" src="/js/isotope.pkgd.min.js"></script>,
   <script key="pace" src="/js/pace.min.js"></script>,
